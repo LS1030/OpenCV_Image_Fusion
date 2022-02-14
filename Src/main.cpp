27f9dc96996f4c5a75410bfefb7352375134d451
L.S <L.S.1030@outlook.com>
2022-02-14 17:57:51 +0800

    Add logic code used split/merge function

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  string input_image1_path = "../Source/Image/input_image1.jpg";
+  string input_image2_path = "../Source/Image/input_image2.jpg";
+  string output_image_path = "../Result/Image/output_image.jpg";
+
+  cv::Mat input_image1 = cv::imread(input_image1_path); //3 channel
+  cv::Mat input_image2 = cv::imread(input_image2_path, IMREAD_GRAYSCALE); //3 channel but only 1 channel has message
+  cv::Mat output_image;
+
+  std::vector<cv::Mat> image1_channels;
+  cv::split(input_image1, image1_channels);
+  cv::Mat image1_channel_b = image1_channels.at(0);
+  cv::Mat image1_channel_g = image1_channels.at(1);
+  cv::Mat image1_channel_r = image1_channels.at(2);
+  cv::add(input_image2, image1_channels.at(0), image1_channels.at(0));
+  cv::add(input_image2, image1_channels.at(1), image1_channels.at(1));
+  cv::add(input_image2, image1_channels.at(2), image1_channels.at(2));
+  cv::merge(image1_channels, output_image);
+
+  cv::imwrite(output_image_path, output_image);
+
   return 0;
 }
