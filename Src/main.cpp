0858c8d2d8c7f897f1c5f2f469645fb7ff9ca898
L.S <L.S.1030@outlook.com>
2022-02-14 18:30:12 +0800

    handle 1 channel used threshold/split/merge function

--- Src/main.cpp
+++ Src/main.cpp
@@ -13,6 +13,8 @@ int main()
 
   cv::Mat input_image1 = cv::imread(input_image1_path); //3 channel
   cv::Mat input_image2 = cv::imread(input_image2_path, IMREAD_GRAYSCALE); //3 channel but only 1 channel has message
+  cv::Mat input_image2_mask;
+  cv::threshold(input_image2, input_image2_mask, 250, 255, THRESH_BINARY_INV);
   cv::Mat output_image;
 
   std::vector<cv::Mat> image1_channels;
@@ -21,8 +23,8 @@ int main()
   cv::Mat image1_channel_g = image1_channels.at(1);
   cv::Mat image1_channel_r = image1_channels.at(2);
   cv::add(input_image2, image1_channels.at(0), image1_channels.at(0));
-  cv::add(input_image2, image1_channels.at(1), image1_channels.at(1));
-  cv::add(input_image2, image1_channels.at(2), image1_channels.at(2));
+  cv::bitwise_and(input_image2_mask, image1_channels.at(1), image1_channels.at(1));
+  cv::bitwise_and(input_image2_mask, image1_channels.at(2), image1_channels.at(2));
   cv::merge(image1_channels, output_image);
 
   cv::imwrite(output_image_path, output_image);
