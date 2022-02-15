9aaaa22e61126ed415965784919d0e300da8b3b3
L.S <L.S.1030@outlook.com>
2022-02-15 12:20:52 +0800

    Add result and time statistic for method2

--- Src/main.cpp
+++ Src/main.cpp
@@ -17,14 +17,20 @@ int main()
 
   time_statistic.start_time = Get_Time_us();
   /* split image1 to C3, add/bitwise_and image2_b/image2_b_mask */
-  cv::Mat input_image2_b_mask;
-  cv::threshold(input_image2_b, input_image2_b_mask, 250, 255, THRESH_BINARY_INV);
-  std::vector<cv::Mat> image1_channels;
-  cv::split(input_image1, image1_channels);
-  cv::add(input_image2_b, image1_channels.at(0), image1_channels.at(0));
-  cv::bitwise_and(input_image2_b_mask, image1_channels.at(1), image1_channels.at(1));
-  cv::bitwise_and(input_image2_b_mask, image1_channels.at(2), image1_channels.at(2));
-  cv::merge(image1_channels, output_image);
+  /* convert image2_b(1channel) to image2(3channels) */
+  cv::Mat input_image2;
+  cv::Mat input_image2_g = Mat::zeros(input_image2_b.rows, input_image2_b.cols, CV_8UC1);
+  cv::Mat input_image2_r = Mat::zeros(input_image2_b.rows, input_image2_b.cols, CV_8UC1);
+  std::vector<cv::Mat> input_image2_bgr;
+  input_image2_bgr.insert(input_image2_bgr.begin(), input_image2_b);
+  input_image2_bgr.insert(input_image2_bgr.end(), input_image2_g);
+  input_image2_bgr.insert(input_image2_bgr.end(), input_image2_r);
+  cv::merge(input_image2_bgr, input_image2);
+
+  /* use mask and bitwise_and to make output_image */
+  cv::Mat input_image2_mask;
+  cv::threshold(input_image2, input_image2_mask, 250, 255, THRESH_BINARY_INV);
+  cv::bitwise_and(input_image2_mask, input_image1, output_image);
 
   time_statistic.end_time = Get_Time_us();
   printf("cost %.3lf ms", (double)(time_statistic.end_time - time_statistic.start_time) / 1000.0);
@@ -33,6 +39,16 @@ int main()
 
   return 0;
 }
+
+// method1
+// cv::Mat input_image2_b_mask;
+// cv::threshold(input_image2_b, input_image2_b_mask, 250, 255, THRESH_BINARY_INV);
+// std::vector<cv::Mat> image1_channels;
+// cv::split(input_image1, image1_channels);
+// cv::add(input_image2_b, image1_channels.at(0), image1_channels.at(0));
+// cv::bitwise_and(input_image2_b_mask, image1_channels.at(1), image1_channels.at(1));
+// cv::bitwise_and(input_image2_b_mask, image1_channels.at(2), image1_channels.at(2));
+// cv::merge(image1_channels, output_image);
 
 // method2
 // /* convert image2_b(1channel) to image2(3channels) */
